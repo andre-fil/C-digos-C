1cd4366dc7274c6197eaf7d194732d18081734a4
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-04-11 10:06:34 -0300

    código vetores

---
+++ vetores.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h> 
+#define TAMANHO 5
+
+int main (void) 
+{ 
+int iIndice;
+int iValorA;
+int iSoma; 
+int aVetor[TAMANHO]; 
+float fMedia;
+
+for (iIndice = 0; iIndice < TAMANHO; iIndice++) 
+{ 
+printf("Entre com o valor %d:", iIndice + 1); 
+scanf("%d", &iValorA); 
+aVetor[iIndice] = iValorA; 
+} 
+
+iSoma = 0; 
+for (iIndice=0; iIndice < TAMANHO; iIndice++) 
+{ 
+iSoma += aVetor[iIndice]; 
+} 
+fMedia = (float) iSoma/TAMANHO; 
+printf ("Media : %f\n", fMedia); 
+return 0; 
+}
