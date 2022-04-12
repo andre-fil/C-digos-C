9a81d846f222fa55f46c43e240694b2fa5ab5dc7
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-04-12 11:31:53 -0300

    padrão de asteriscos usando for

---
+++ lista de exercícios/asteriscos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) 
+{ 
+int num = 10;
+char ast = '*';
+
+for (int i = num; i>= 1;i--){
+  for(int j = i; j>=1;j--){
+    printf("*");
+  }
+  printf("\n");
+  }
+  }
